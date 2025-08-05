a6c737eb4bd589960e8dfa621652a874702dec58
stomaras <sptoma724@gmail.com>
2025-08-05 23:07:48 +0300

    add lazy loading with suspense

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,34 +3,43 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import ProfileList from "./components/ProfileList";
-import Authors from "./components/Authors";
 import CreateBookForm from "./components/CreateBookForm"; // 👈 your book form
 import StudentList from "./components/StudentList";
-import StudentDetails from "./components/StudentDetails";
 import Login from "./components/Login";
 import ProtectedRoute from "./components/ProtectedRoute";  // import your ProtectedRoute
+import ErrorBoundary from "./components/ErrorBoundary";
+import { Suspense, lazy } from "react";
 
-
+const Authors = lazy(() => import('./components/Authors'));
+const StudentDetails = lazy(() => import('./components/StudentDetails'));
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>
-        <Route path="/profiles" element={<ProfileList />} />
-        <Route path="/authors" element={<Authors />} />
-        <Route path="/books/new" element={<CreateBookForm />} /> {/* 👈 new route */}
-        {/* Wrap StudentList with ProtectedRoute */}
-        <Route
-          path="/students"
-          element={
-            <ProtectedRoute>
-              <StudentList />
-            </ProtectedRoute>
-          }
-        />        <Route path="/students/:id" element={<StudentDetails />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Routes>
+          <Route path="/profiles" element={<ErrorBoundary fallback={<div>Oops! Could not load students. Try refreshing.</div>}>
+              <ProfileList />                  
+            </ErrorBoundary>            
+          } />
+          <Route path="/authors" element={<Authors />} />
+          <Route path="/books/new" element={<CreateBookForm />} /> {/* 👈 new route */}
+          {/* Wrap StudentList with ProtectedRoute */}
+          <Route
+            path="/students"
+            element={
+              <ProtectedRoute>
+                    <ErrorBoundary fallback={<div>Oops! Could not load students. Try refreshing.</div>}>
+                      <StudentList />
+                    </ErrorBoundary>            
+              </ProtectedRoute>
+            }
+          />        
+          <Route path="/students/:id" element={<StudentDetails />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
